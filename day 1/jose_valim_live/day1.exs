0a0b3673a961be1e621e7a0bf398d40b1bda77e1
romenigld <romenigld@gmail.com>
2018-12-03 15:24:37 +0100

    Now using Enum

--- day 1/jose_valim_live/day1.exs
+++ day 1/jose_valim_live/day1.exs
@@ -2,18 +2,13 @@ defmodule Day1 do
   def final_frequency(input) do
     input
     |> String.split("\n", trim: true)
-    |> sum_lines(0)
-  end
-
-  defp sum_lines([line | lines], current_frequency) do
-    new_frequency = String.to_integer(line) + current_frequency
-    sum_lines(lines, new_frequency)
-  end
-
-  defp sum_lines([], current_frequency) do
-    current_frequency
+    |> IO.inspect(label: "split")
+    |> Enum.map(fn line -> String.to_integer(line) end)
+    |> IO.inspect(label: "mapped")
+    |> Enum.sum()
   end
 end
+
 case System.argv() do
   ["--test"] ->
     ExUnit.start()
