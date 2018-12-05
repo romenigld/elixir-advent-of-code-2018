d983846248efa14e919f8b4532290deb383f6822
romenigld <romenigld@gmail.com>
2018-12-05 16:03:12 +0100

    doing more verbose with recursion in binaries

--- day 2/day2/lib/day2.ex
+++ day 2/day2/lib/day2.ex
@@ -1,10 +1,15 @@
 defmodule Day2 do
 
   def count_characters(string) when is_binary(string) do
-    string
-    |> String.to_charlist()
-    |> Enum.reduce(%{}, fn codepoint, acc ->
-      Map.update(acc, codepoint, 1, &(&1+1))
-    end)
+    count_characters(string, %{})
+  end
+
+  defp count_characters(<<byte, rest::binary>>, acc) do
+    acc = Map.update(acc, byte, 1, &(&1 +1))
+    count_characters(rest, acc)
+  end
+
+  defp count_characters(<<>>, acc) do
+    acc
   end
 end
