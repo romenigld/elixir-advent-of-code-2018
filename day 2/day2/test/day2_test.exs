b74f84a79c2c30b64bce4f75159e2dee7a091906
romenigld <romenigld@gmail.com>
2018-12-06 14:27:20 +0100

    do the checksum method and the function for get twice and thrice

--- day 2/day2/test/day2_test.exs
+++ day 2/day2/test/day2_test.exs
@@ -4,10 +4,28 @@ defmodule Day2Test do
   doctest Day2
 
   test "count characters" do
+    assert Day2.count_characters("aabbcc") == %{
+      ?a => 2,
+      ?b => 2,
+      ?c => 2
+    }
+
     assert Day2.count_characters("éaabbé") == %{
       ?a => 2,
       ?b => 2,
       ?é => 2
     }
+  end
+
+  test "checksum" do
+    assert Day2.checksum([
+            "abcdef",
+            "bababc",
+            "abbcde",
+            "abcccd",
+            "aabcdd",
+            "abcdee",
+            "ababab",
+          ]) == 12
   end
 end
