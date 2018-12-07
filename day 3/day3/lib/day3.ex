85cdcb950a7cfa526030149c255768e0b8d6a4a3
romenigld <romenigld@gmail.com>
2018-12-07 14:43:50 +0100

    Created the @ type's and @ spec's  and the function claimed_inches using Enum.reduce inside of other reduces

--- day 3/day3/lib/day3.ex
+++ day 3/day3/lib/day3.ex
@@ -1,4 +1,9 @@
 defmodule Day3 do
+  @type claim :: String.t
+  @type parsed_claim :: list
+  @type coordinate :: {pos_integer, pos_integer}
+  @type id :: integer
+
   @doc """
   Parses a claim.
 
@@ -8,9 +13,36 @@ defmodule Day3 do
     [1, 100, 366, 24, 27]
 
   """
+  @spec parse_claim(claim) :: parsed_claim
   def parse_claim(string) when is_binary(string) do
     string
     |> String.split(["#", " @ ", ",", ": ", "x"], trim: true)
     |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Retrives all claimed inches.
+
+  ## Examples
+
+    iex> claimed = Day3.claimed_inches([
+    ...>  "#1 @ 1,3: 4x4",
+    ...>  "#2 @ 3,1: 4x4",
+    ...>  "#3 @ 5,5: 2x2",
+    ...>])
+    iex> claimed[{4, 2}]
+    [2]
+  """
+  @spec claimed_inches([claim]) :: %{coordinate => [id]}
+  def claimed_inches(claims) do
+    Enum.reduce(claims, %{}, fn claim, acc ->
+      [id, left, top, width, height] = parse_claim(claim)
+
+      Enum.reduce((left + 1)..(left + width), acc, fn x, acc ->
+        Enum.reduce((top + 1)..(top + height), acc, fn y, acc ->
+          Map.update(acc, {x, y}, [id], &[id | &1])
+        end)
+      end)
+    end)
   end
 end
